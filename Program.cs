ae6b684e523b193761b95739c8b23a385c69bd05
mohamed lamrani <mohamed168@hotmail.fr>
2021-10-12 01:28:15 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PremierNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Enter a Number");
+            int number;
+            int temp = 0;
+            int i;
+            int.TryParse(Console.ReadLine(), out number);
+            
+            for (i = 1; i <= number; i++)
+            {
+                
+                if (number % i == 0)
+                    temp = temp + 1;
+            }
+            if (temp == 2)
+                Console.WriteLine("The number you enter is premier");
+            else
+                Console.WriteLine("The number you enter is not premier");
+
+            Console.ReadKey();
+
+        }
+    }
+}
